5d1491e53cc2d1a8613038278d9191bab16757fc
Rushil Perera <pererarushil1940@gmail.com>
2017-05-21 08:48:56 -0400

    Added config file

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by silver_android on 5/21/2017.
+ */
+require('dotenv').config();
+
+module.exports = {
+    development: process.env.DB_PATH,
+    test: process.env.TEST_DB_PATH
+};
