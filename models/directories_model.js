e9ba398a31fcf0db3fab4e52b29e1ca9f9cc6cd4
Rushil Perera <pererarushil1940@gmail.com>
2017-05-21 08:54:13 -0400

    Added new method

--- models/directories_model.js
+++ models/directories_model.js
@@ -14,3 +14,22 @@ module.exports.getDirectories = async () => {
     }
     return directories.map(directory => directory.path);
 };
+
+module.exports.add = async (directory) => {
+    try {
+        await db.run('INSERT INTO directories (path) VALUES (?);', [directory]);
+    } catch (e) {
+        if (e.message.indexOf('UNIQUE constraint failed') > -1) {
+            if (e.message.indexOf('directories.path') > -1) {
+                console.log(`${directory} already added`);
+            } else {
+                console.error(e);
+                return {error: true};
+            }
+        } else {
+            console.error(e);
+            return {error: true};
+        }
+    }
+    return {error: false};
+};
