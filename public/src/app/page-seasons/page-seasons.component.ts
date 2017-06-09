13292c61cf65cd46cb5bd15560b1cc59aabede72
Rushil Perera <pererarushil1940@gmail.com>
2017-06-09 07:09:05 -0400

    Displays show info at top of page

--- public/src/app/page-seasons/page-seasons.component.ts
+++ public/src/app/page-seasons/page-seasons.component.ts
@@ -3,6 +3,7 @@ import {ShowsService} from "../shows.service";
 import {ActivatedRoute, Params} from "@angular/router";
 import {Location} from "@angular/common";
 import {Season} from "../season";
+import {Show} from "../show";
 
 @Component({
     selector: 'app-page-show',
@@ -14,6 +15,7 @@ export class PageSeasonsComponent implements OnInit {
     private _numCols: number;
     private _showID: number;
     rows: Season[][];
+    show: Show;
     loading: boolean;
 
     constructor(private _showsService: ShowsService, private _route: ActivatedRoute) {
@@ -28,7 +30,10 @@ export class PageSeasonsComponent implements OnInit {
                 this._showID = +params.id;
                 return this._showsService.getSeasons(this._showID);
             })
-            .subscribe(seasons => {
+            .subscribe(res => {
+                const show = res.show.data;
+                this.show = new Show(show.name, show.image_url, show.overview);
+                const seasons = res.seasons.data;
                 seasons.forEach((season, i) => {
                     if (i % this._numCols === 0)
                         this.rows.push([]);
--- public/src/app/show/show.component.ts
+++ public/src/app/show/show.component.ts
@@ -10,11 +10,23 @@ export class ShowComponent implements OnInit {
 
     @Input('data')
     show: Show;
+    @Input()
+    expanded: boolean;
+    overviewClass;
+    imgStyle;
 
     constructor() {
     }
 
     ngOnInit() {
+        if (this.expanded) {
+            this.overviewClass = '';
+            this.imgStyle = {width: '100px', margin: '0 16px 0 0', float: 'left'};
+        }
+        else {
+            this.overviewClass = 'overview';
+            this.imgStyle = {};
+        }
     }
 
 }
