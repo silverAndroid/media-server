d72e5227a958bb4661dbbc60941e35d2a4962bab
Rushil Perera <pererarushil1940@gmail.com>
2017-07-13 09:53:27 -0400

    Does not automatically listen for file system changes when DB is initialized

--- models/db.js
+++ models/db.js
@@ -5,12 +5,10 @@ const db = require('sqlite');
 const Promise = require('bluebird');
 
 const config = require('../config');
-const fileListen = require('../file_listen');
 
 module.exports.init = async () => {
     try {
         await db.open(config[process.env.NODE_ENV], {Promise});
-        await fileListen.init();
     } catch (e) {
         console.error(e.stack);
     }
