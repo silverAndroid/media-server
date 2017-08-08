2ff8dd054bce290d94b4f3e1650819c4e842c6a5
Rushil Perera <pererarushil1940@gmail.com>
2017-08-07 21:42:27 -0400

    ffmpeg methods now use the fluent-ffmpeg package now

--- video_processing.js
+++ video_processing.js
@@ -1,9 +1,9 @@
 /**
  * Created by silver_android on 6/12/2017.
  */
-const childProcess = require('child_process');
 const fs = require('fs');
 const promiseLimit = require('promise-limit');
+const ffmpeg = require('fluent-ffmpeg');
 
 const limit = promiseLimit(Number(process.env.MAX_FFMPEG_PROCESSES));
 
@@ -61,49 +61,45 @@ module.exports.convert = paths => new Promise((resolve, reject) => {
 
 function splitFiles(path) {
     return new Promise((resolve, reject) => {
-        // video duration in seconds
         const pathNoExt = util.removeFileExtension(path);
         const extension = util.getFileExtension(path);
         const fileName = `${pathNoExt}_%04d.${extension}`;
-        const command = process.env.FFMPEG_PATH;
-        const args = ['-i', path, '-c', 'copy', '-map', 0, '-segment_time', chunkSize, '-f', 'segment', fileName];
-        // noinspection JSPotentiallyInvalidConstructorUsage
         const files = [];
         const fileSearchRegex = /Opening '(.+)' for writing/g;
         let message = '';
 
-        console.log(`Spawning ${command} ${args.join(' ')}`);
-        const child = childProcess.spawn(command, args);
-        child.stderr.setEncoding('utf-8');
-        child.stderr.on('data', (data) => {
-            message += data;
-            let file;
-            let index;
-            // eslint-disable-next-line no-cond-assign
-            while (file = fileSearchRegex.exec(message)) {
-                files.push(file[1]);
-                index = file.index;
-            }
-            message = message.slice(index).replace(fileSearchRegex, '');
-            console.error(data);
-        });
-        child.on('exit', (code, signal) => {
-            if (!signal) {
-                if (code === 0) {
-                    console.log(`Successfully chunked ${path}`);
-                    fs.writeFile(`${pathNoExt}.txt`, files.map(file => `${util.removeFileExtension(file)}.mp4`).join('\n'), (err) => {
-                        if (err) {
-                            reject(err);
-                        }
-                        resolve(files);
-                    });
-                } else {
-                    reject(`Exit code ${code} sent!`);
+        ffmpeg(path)
+            .outputOptions([
+                '-c copy',
+                '-map 0',
+                `-segment_time ${chunkSize}`,
+            ])
+            .output(fileName)
+            .format('segment')
+            .on('start', command => console.log(`Spawning ${command}`))
+            .on('stderr', (data) => {
+                message += data;
+                let file;
+                let index;
+                // eslint-disable-next-line no-cond-assign
+                while (file = fileSearchRegex.exec(message)) {
+                    files.push(file[1]);
+                    index = file.index;
                 }
-            } else {
-                reject(`Process signal ${signal} sent!`);
-            }
-        });
+                message = message.slice(index).replace(fileSearchRegex, '');
+                console.error(data);
+            })
+            .on('error', err => reject(err.message))
+            .on('end', () => {
+                console.log(`Successfully chunked ${path}`);
+                fs.writeFile(`${pathNoExt}.txt`, files.map(file => `${util.removeFileExtension(file)}.mp4`).join('\n'), (err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(files);
+                });
+            })
+            .run();
     });
 }
 
@@ -111,73 +107,40 @@ function encode(path) {
     return new Promise((resolve, reject) => {
         const pathNoExt = util.removeFileExtension(path);
         const fileName = util.getFileName(path);
-        const command = process.env.FFMPEG_PATH;
-        const args = ['-i', path, '-f', 'mp4', '-vcodec', 'libx264', '-preset', 'fast', '-speed', 8, '-profile:v', 'main', '-acodec', 'aac', `${pathNoExt}.mp4`, '-hide_banner'];
-        console.log(`Encoding ${path} to ${pathNoExt}.mp4`);
-        console.log(`Spawning ${command} ${args.join(' ')}`);
-        const child = childProcess.spawn(command, args);
         let message = '';
 
-        child.stdin.setEncoding('utf-8');
-        child.stdout.setEncoding('utf-8');
-        child.stderr.setEncoding('utf-8');
-        child.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-        });
-        child.stderr.on('data', (data) => {
-            message += data;
-            if (message.indexOf('Overwrite ?') > -1) {
-                child.stdin.write('y\n');
-                child.stdin.end();
-                message = '';
-            }
-            // console.error(data);
-        });
-        child.on('exit', (code, signal) => {
-            if (!signal) {
-                if (code === 0) {
-                    resolve();
-                    console.log(`Successfully encoded ${fileName}`);
-                    fs.unlink(path, (err) => {
-                        if (err) {
-                            console.error(err);
-                        }
-                    });
-                } else {
-                    fs.unlink(`${pathNoExt}.mp4`, err => console.error(err));
-                    reject(`Exit code ${code} sent!`);
+        ffmpeg(path)
+            .outputOptions([
+                '-preset fast',
+                '-speed 8',
+                '-profile:v main',
+                '-hide_banner',
+            ])
+            .audioCodec('aac')
+            .videoCodec('libx264')
+            .format('mp4')
+            .on('start', (command) => {
+                console.log(`Encoding ${path} to ${pathNoExt}.mp4`);
+                console.log(`Spawning ${command}`);
+            })
+            .on('error', err => reject(err))
+            .on('stderr', (data) => {
+                message += data;
+                if (message.indexOf('Overwrite ?') > -1) {
+                    // child.stdin.write('y\n');
+                    // child.stdin.end();
+                    message = '';
                 }
-            } else {
-                fs.unlink(`${pathNoExt}.mp4`, err => console.error(err));
-                reject(`Process signal ${signal} sent!`);
-            }
-        });
-    });
-}
-
-function getDuration(path) {
-    return new Promise((resolve, reject) => {
-        const command = process.env.FFPROBE_PATH;
-        const args = ['-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', path];
-        let duration = '';
-
-        console.log(`Getting duration of ${path}`);
-        console.log(`Spawning ${command} ${args.join(' ')}`);
-
-        const child = childProcess.spawn(command, args);
-        child.stdout.on('data', (data) => {
-            duration += data;
-        });
-        child.on('exit', (code, signal) => {
-            if (!signal) {
-                if (code === 0) {
-                    resolve(Number(duration));
-                } else {
-                    reject(`Exit code ${code} sent!`);
-                }
-            } else {
-                reject(`Process signal ${signal} sent!`);
-            }
-        });
+            })
+            .on('end', () => {
+                resolve();
+                console.log(`Successfully encoded ${fileName}`);
+                fs.unlink(path, (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+            })
+            .save(`${pathNoExt}.mp4`);
     });
 }
