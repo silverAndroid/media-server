51be9df033090ba19c50707d9a68a3beeaa5ee68
Rushil Perera <pererarushil1940@gmail.com>
2017-08-15 11:21:33 -0400

    Added utility methods for file chunks

--- util.js
+++ util.js
@@ -9,6 +9,10 @@ module.exports.getFileName = path => path.split(/[/\\]/).pop();
 
 module.exports.isChunkedVideo = path => module.exports.getFileName(path).match(/.+_\d{4,}\./) !== null;
 
+module.exports.getChunkNumber = path => Number(module.exports.getFileName(path).match(/.+_(\d{4,})\./)[1]);
+
+module.exports.removeChunkEnding = path => (module.exports.isChunkedVideo(path) ? path.replace(path.match(/(_\d{4,})\.[^/.]+$/)[1], '') : path);
+
 module.exports.getParentFolder = (path) => {
     const parentFolder = path.split(/[/\\]/);
     parentFolder.pop();
